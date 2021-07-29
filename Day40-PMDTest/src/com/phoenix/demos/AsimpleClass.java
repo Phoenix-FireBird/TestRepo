53b32cb6784347ef7116d88b0c23a63180270730
Phoenix-FireBird <phoenix.firebird@gmail.com>
2021-07-29 13:02:23 +0530

    Day40 project

---
+++ Day40-PMDTest/src/com/phoenix/demos/AsimpleClass.java
@@ -0,0 +1,16 @@
+package com.phoenix.demos;
+
+/**
+ * 
+ * @author admin
+ *
+ */
+
+public class AsimpleClass {
+
+	public static void main(String[] args) {
+		/* This is the main method */
+
+	}
+
+}
